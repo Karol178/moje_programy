7db855c43c459938e284597aa7c4e25e2805283c
Karol178 <karol.przeszlo2@gmail.com>
2024-05-28 18:58:21 +0200

    Add files via upload
    program w java liczący liczbę pi

---
+++ licz_pi.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float pi;
+        float mnoznik = 4.0f;
+
+        pi = mnoznik;
+        int dokladnosc = 100000000;
+        int licznik = 0;
+        float mianownik = 3;
+
+
+        while(licznik <= dokladnosc) {
+            pi -= (mnoznik) / mianownik;
+            mianownik += 2;
+            pi += (mnoznik) / mianownik;
+            mianownik += 2;
+            licznik++;
+
+        }
+        pi = (pi*4)/mnoznik;
+        System.out.println(pi);
+    }
+}
